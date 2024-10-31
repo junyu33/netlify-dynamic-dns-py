336d4a3904da79aa0eb782aee03d19eeeefbe3ff
junyu33 <2658799217@qq.com>
2024-10-31 11:46:51 +0800

    feat: add example of config.sh

---
+++ config.sh
@@ -0,0 +1,3 @@
+ACCESSTOKEN="<your_access_token>"  # Netlify API access token
+ZONE="<your_root_domain>"          # Domain name (Netlify DNS zone)
+RECORD="<your_subdomain>"          # Subdomain (rdp.junyu33.me)ource ./config.sh
