692933f08ef6da3b39d1227eefcd1a3684f43eb6
junyu33 <37526026+junyu33@users.noreply.github.com>
2024-10-31 12:57:19 +0800

    docs: clarify the requirements of config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 ACCESSTOKEN="<your_access_token>"  # Netlify API access token
-ZONE="<your_root_domain>"          # Domain name (Netlify DNS zone)
-RECORD="<your_subdomain>"          # Subdomain (rdp.junyu33.me)ource ./config.sh
+ZONE="example.com"          # Domain name (example.com)
+RECORD="foo"          # Subdomain (foo for foo.example.com)
